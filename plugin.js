de81384849adce65dee7508317c8bc39fc1b656d
KYAW THET KHINE LIN <kyawthetkhinelin@innovix-solutions.com>
2020-02-13 15:50:40 +0630

    code add

---
+++ plugin.js
@@ -0,0 +1,38 @@
+$.fn.adjustHeight = function(options) {
+  var settings = $.extend({
+        minHeight : null, //optional
+    }, options );
+  var sameHeightGlobalData = {};
+  this.each(function () {    
+    var height = $(this).outerHeight();
+    var offset = $(this).offset();
+    var key = "p".concat(offset.top);
+    if (sameHeightGlobalData.hasOwnProperty(key)) {
+      if (sameHeightGlobalData[key] < height) {
+        sameHeightGlobalData[key] = height;
+      }
+    } else {
+      sameHeightGlobalData[key] = height;
+    }
+  });
+  console.log(this);
+  this.each(function () {
+    var offset = $(this).offset();
+    var key = "p".concat(offset.top);
+      if(sameHeightGlobalData.hasOwnProperty(key)) {
+        $(this).addClass(key);             
+      }
+  });
+
+  $.each(sameHeightGlobalData,function(i,v){
+    i = "."+i;
+    if(settings.minHeight != null){
+      if(settings.minHeight < v)
+      $(i).css('height', v);
+      else
+      $(i).css('height', settings.minHeight);
+    }else{
+      $(i).css('height', v);
+    }   
+  });
+}
